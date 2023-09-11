3039bc6b6b349eb534859429c02d96db470eb147
Konstantin Yan <108539449+Yankeegohome@users.noreply.github.com>
2023-09-11 22:52:01 +0300

    Коммит из cmd

---
+++ FirstOne.java
@@ -0,0 +1,10 @@
+public class FirstOne {
+    String model;
+    int maxSpeed;
+    // Иницилизация класса
+    public FirstOne (String model, int maxSpeed) {
+        this.model = model;
+        this.maxSpeed = maxSpeed;
+
+    }
+}
---
+++ StudyGit.java
@@ -0,0 +1,10 @@
+
+public class StudyGit {
+    public static void main (String[] args) {
+        System.out.println("He");
+        // Обьявление класса
+        FirstOne buggati = new FirstOne("Buggati", 400);
+        System.out.println(buggati);
+    }
+}
+
