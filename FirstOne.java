353e28218bb4a85377cd27b48af678eea54c925e
Konstantin Yan <108539449+Yankeegohome@users.noreply.github.com>
2023-09-11 22:58:37 +0300

    my second commit

--- FirstOne.java
+++ FirstOne.java
@@ -1,10 +1,13 @@
 public class FirstOne {
     String model;
     int maxSpeed;
+    String caseType;
     // Иницилизация класса
-    public FirstOne (String model, int maxSpeed) {
+    public FirstOne (String model, int maxSpeed, String caseType) {
         this.model = model;
         this.maxSpeed = maxSpeed;
+        this.caseType = caseType;
+
 
     }
 }
--- StudyGit.java
+++ StudyGit.java
@@ -3,8 +3,9 @@ public class StudyGit {
     public static void main (String[] args) {
         System.out.println("He");
         // Обьявление класса
-        FirstOne buggati = new FirstOne("Buggati", 400);
+        FirstOne buggati = new FirstOne("Buggati", 400, "SportCar");
         System.out.println(buggati);
+        System.out.println("second commit" + buggati);
     }
 }
 
